b1cc2515ff7418b4fde435be417e49597015b910
JSampereVázquez <72076376+jsamperevazquez@users.noreply.github.com>
2025-09-28 07:47:06 +0200

    script operadores created

---
+++ www/practica_1/operadores.js
@@ -0,0 +1,25 @@
+let a = 10
+let b = "10"
+let c = 15
+let d = 20
+
+console.log(a == b)
+console.log(a != c)
+console.log(a === b)
+console.log(a !== b)
+console.log(d >= c)
+console.log(a < c)
+console.log(a <= b)
+
+let x = 10
+let y = 3
+
+console.log(x + y)
+console.log(x - y)
+console.log(x * y)
+console.log(x / y)
+console.log(x % y)
+x++
+console.log(x)
+x--
+console.log(x)
