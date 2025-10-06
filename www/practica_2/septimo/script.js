5faa73c1d0a8e08e7cff4b9b3a114a1d5c500fe3
JSampereVázquez <72076376+jsamperevazquez@users.noreply.github.com>
2025-10-06 21:02:10 +0200

    function mostrarMatrizTabla created

--- www/practica_2/septimo/script.js
+++ www/practica_2/septimo/script.js
@@ -1,13 +1,13 @@
-function generarMatriz(n1, n2){
-    function aleatorio(min, max){
+function generarMatriz(n1, n2) {
+    function aleatorio(min, max) {
         let numero = Math.floor(Math.random() * (max - min + 1) + min)
         return numero
     }
-    
+
     let matriz = []
-    for (let i=0; i < n1; i++ ){
+    for (let i = 0; i < n1; i++) {
         let fila = []
-        for (let j= 0; j < n2; j++){
+        for (let j = 0; j < n2; j++) {
             fila.push(Math.floor(aleatorio(10, 100)))
         }
         matriz.push(fila)
@@ -15,8 +15,18 @@ function generarMatriz(n1, n2){
     return matriz
 }
 
-function mostrarMatrizTabla(){
-    // TODO
+function mostrarMatrizTabla() {
+    let fila = 3
+    let columna = 3
+    let tabla = '<table>'
+    const matriz = generarMatriz(fila, columna)
+    for (let i = 0; i < fila; i++) {
+        tabla += '<tr>'
+        for (let j = 0; j < columna; j++) {
+            tabla += `<td>${matriz[i][j]}</td>`
+        }
+        tabla += '</tr>'
+    }
+    tabla += '</table>'
+    document.getElementById("resultado").innerHTML = tabla
 }
-
-console.log(generarMatriz(2, 2))
