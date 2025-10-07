9a6f42eb42c913568c471c186e84c4d7dd5ad399
JSampereVázquez <72076376+jsamperevazquez@users.noreply.github.com>
2025-10-07 12:29:38 +0200

    Exercicio 7 finished

--- www/practica_2/septimo/script.js
+++ www/practica_2/septimo/script.js
@@ -1,6 +1,18 @@
+/**
+ * Funcion que xera unha matriz aleatoria coas dimensións que se lle pasa por parámetros
+ * @param {number} n1 Filas da matriz
+ * @param {number} n2 Columnas da matriz
+ * @returns {[number]} Matriz numérica aleatoria
+ */
 function generarMatriz(n1, n2) {
+    /**
+     * Función que xera números aleatorios
+     * @param {number} min O número para o rango mínimo desexado
+     * @param {number} max O número para o rango máximo desexado
+     * @returns {number} O número aleatorio creado
+     */
     function aleatorio(min, max) {
-        let numero = Math.floor(Math.random() * (max - min + 1) + min)
+        let numero = Math.floor(Math.random() * (max - min + 1) + min) //xera número aleatorio entre rangos
         return numero
     }
 
@@ -8,22 +20,43 @@ function generarMatriz(n1, n2) {
     for (let i = 0; i < n1; i++) {
         let fila = []
         for (let j = 0; j < n2; j++) {
-            fila.push(Math.floor(aleatorio(10, 100)))
+            fila.push(Math.floor(aleatorio(10, 100))) // Va ingresando na fila o número xerado na función
         }
-        matriz.push(fila)
+        matriz.push(fila) // Ingresa a fila na matriz
     }
     return matriz
 }
-
+/**
+ * Función para xerar una matriz resultante da suma de duas matrices recibidas por parámetroa
+ * @param {[number]} m1 Matriz aleatoria 
+ * @param {[number]} m2  Matriz aleatoria
+ * @returns {[number]} Matriz suma da m1 + m2
+ */
+function sumMatrix(m1, m2){
+    let sumaMatrix = []
+    for (let i = 0; i < m1.length; i ++){
+        let fila = []
+        for(let j = 0; j < m2.length; j ++){
+            fila.push(m1[i][j] + m2[i][j])
+        }
+        sumaMatrix.push(fila)
+    }
+    return sumaMatrix
+}
+/**
+ * Función que renderiza o resultado da matriz suma no DOM html
+ */
 function mostrarMatrizTabla() {
-    let fila = 3
-    let columna = 3
-    let tabla = '<table>'
-    const matriz = generarMatriz(fila, columna)
+    let fila = 3 // Número de filas que queremos na matriz 
+    let columna = 3 // Número de columnas que queremos na matriz
+    let tabla = '<table>' // Creo a etiqueta HTML da tabla
+    const MATRIZ_1 = generarMatriz(fila, columna)
+    const MATRIZ_2 = generarMatriz(fila, columna)
+    const MATRIZ_SUM = sumMatrix(MATRIZ_1, MATRIZ_2)
     for (let i = 0; i < fila; i++) {
         tabla += '<tr>'
         for (let j = 0; j < columna; j++) {
-            tabla += `<td>${matriz[i][j]}</td>`
+            tabla += `<td>${MATRIZ_SUM[i][j]}</td>` // Interpolación para crear os valores das columnas
         }
         tabla += '</tr>'
     }
