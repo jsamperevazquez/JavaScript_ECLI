622d565f34c581b49bc80f239cbe076eb0c8e503
JSampereVázquez <72076376+jsamperevazquez@users.noreply.github.com>
2025-10-22 17:13:32 +0200

    New funtion add

--- www/practicas_extras/script.js
+++ www/practicas_extras/script.js
@@ -105,3 +105,59 @@ let mayorNumeros = (num1, num2) => {
 };
 
 mayorNumeros(10, 20);
+
+/*
+Sistema de facturación
+
+Crear una función que se llame facturacion() .
+La función tiene que recibir como parámetro el monto de un producto ,
+y el medio de pago : C (tarjeta de crédito), E (efectivo)
+y D (tarjeta de débito). Si el monto del producto es menor a $200 no se aplicará ningún descuento. 
+Si el monto a abonar es entre $200 y $400 se aplicará un descuento del 30%
+si el medio de pago es efectivo, 20% si se realiza con dÃ©bito y 10% con tarjeta de crédito. 
+Para montos mayores a $400, el descuento es 40% sin importar el medio de pago.
+La función deberá retornar el monto final a abonar.
+*/
+
+let facturacion = function (monto, medioPago) {
+    if (monto < 200){
+        return monto;
+    }
+    if (monto >= 200 && monto <= 400) {
+        switch (medioPago) {
+            case 'E':
+                return monto * 0.7;
+            case 'D':
+                return monto * 0.8;
+            case 'C':
+                return monto * 0.9;
+            default:
+                console.log("Medio de pago no válido");
+                return monto;
+
+        }
+    } else if (monto > 400) {
+        return monto * 0.6;
+    }
+};
+console.log("El monto final a abonar es: €" + facturacion(150, 'E'));
+
+
+/*
+Creando un método de iteración de array.
+
+Crear una función IterarArray() y pasale el parámetro array.
+Codea dentro de la función la iteración/recorrido de un array y muéstralo por consola.
+Invoca la función IterarArray() y pasale como argumento 3 arrays distintos.
+*/
+let numeroArray = 1;
+let IterarArray = function (miArray){
+    
+    for (let i = 0; i < miArray.length; i++) {
+        console.log(`Elemento ${i} del array ${numeroArray}: ` + miArray[i]);
+    }
+     numeroArray++;
+}
+IterarArray(['rojo', 'verde', 'azul']);
+IterarArray([1, 2, 3, 4, 5]);
+IterarArray(['lunes', 'martes', 'miércoles', 'jueves', 'viernes']); 
